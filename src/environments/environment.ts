6010087ca298ad3e7175976b5b8b5e6b18188c1c
grainneRegan <81359606+grainneRegan@users.noreply.github.com>
2023-01-26 12:17:38 +0000

    Adding environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'fyp2-d3d49',
+    appId: '1:592026899198:web:02d85d3264073c353d8baf',
+    databaseURL: 'https://fyp2-d3d49-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'fyp2-d3d49.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCvvrIko3GfUcEMB25wP_MsnkckS-wieR4',
+    authDomain: 'fyp2-d3d49.firebaseapp.com',
+    messagingSenderId: '592026899198',
+    measurementId: 'G-VQL3PLJPRF',
+  },
+};
