07d77b7411adc0433b6058b280ba9e9cc00c25ac
grainneRegan <81359606+grainneRegan@users.noreply.github.com>
2023-02-23 18:10:51 +0000

    Yay!

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,32 +8,32 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-   title = 'FYP';
-      //Form variables
-    registerForm:any = FormGroup;
-    submitted = false;
-    constructor( private formBuilder: FormBuilder, private router:Router){}
-    //Add user form actions
-    get f() { return this.registerForm.controls; }
-    onSubmit() {
+  title = 'FYP';
+    //Form variables
+  registerForm:any = FormGroup;
+  submitted = false;
+  constructor( private formBuilder: FormBuilder, private router:Router){}
+  //Add user form actions
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
 
-      this.submitted = true;
-      // stop here if form is invalid
-      if (this.registerForm.invalid) {
-          return;
-      }
-      //True if all the fields are filled
-      if(this.submitted)
-      {
-        alert("Great!!");
-      }
-
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
     }
-      ngOnInit() {
-        //Add User form validations
-        this.registerForm = this.formBuilder.group({
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required]]
-        });
-      }
+    //True if all the fields are filled
+    if(this.submitted)
+    {
+      alert("Great!!");
+    }
+
+  }
+  ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+    });
+  }
 }
